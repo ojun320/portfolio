2110a5b57373ba482677e2734fbaeb58a501c568
ojun320 <32353717+ojun320@users.noreply.github.com>
2018-02-01 10:52:59 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,109 @@
+var gApp = angular.module('gApp', []);
+    gApp.run(function($rootScope){
+    	$rootScope.address = "서울시 금천구 가산디지털2로 115 대륭테크노타운3차";
+    	$rootScope.dns = "GooDee";
+    	$rootScope.title = "Portfolio";
+    	$rootScope.name = "한승준";
+    });
+	gApp.controller('gCtrl', function($scope) {
+		$scope.htmlCheck = false;
+		$scope.bodyCheck = false;
+		$scope.btCheck = false;
+		$scope.projectFlag = false;
+		$scope.projectUrl = "";
+		$scope.btnActive = 1;
+		
+		$scope.dropEvent = function() {
+			$scope.htmlCheck = !$scope.htmlCheck;
+			$scope.bodyCheck = !$scope.bodyCheck;
+			$scope.btCheck   = !$scope.btCheck;
+		};
+		
+		$scope.projectEvent = function(rows) {
+			$scope.row = rows;
+			if($scope.projectUrl == rows.url) {
+				$scope.projectUrl = "";
+				$scope.projectFlag = false;
+			} else {
+				$scope.projectUrl = rows.url;
+				$scope.projectFlag = true;
+			}
+		}
+		
+		$scope.iFrameLink = function(){
+			if($scope.iframeView){
+				location.href = $scope.iframeView;
+			}
+		}
+		
+		$scope.btnList = [
+			{filter: "*",      name: "All",      active: true },
+			{filter: ".bgOn",  name: "Personal", active: false},
+			{filter: ".bgOff", name: "Team",     active: false}
+		];
+		
+		$scope.dataSource = [
+			{
+			 path: "portfolio/",
+			 url : "team/team.pdf", 
+			 title: "Team",
+			 name: "Impression",
+			 img: "team/TeamImpression.JPG",
+			 type : true, 
+			 contents: "교육기관에 들어와서 처음 해본 프로젝트입니다. Spring Java 기반으로 만들었으며, 기존 사이트인 Vacheron Constantin 사이트를 벤치마킹하여 한달 반정도의 시간을 갖고 기획부터 시작하여 개발 테스트까지 모두 끝마쳤습니다. 팀프로젝트였기에 각자 역할을 분담하여 계획표에 따라 진행하였으며, 중간중간 점검을 하며 프로젝트 발표일에 맞추어 완성하여 발표까지 하였습니다. 하면서 느낀점은 개인이 아닌 팀프로젝트였기에 서로의 의견조율과 시간준수,현 진행상황보고가 중요하다는 것을 느꼈습니다."
+			},{
+			 path: "portfolio/",
+			 url : "personal/personal.pdf", 
+			 title: "Personal",
+			 name: "Impression",
+			 img: "personal/PersonalImpression.JPG",
+			 type : false,
+			 contents: "교육기관에서 교육을 받고 처음 진행한 개인 프로젝트 입니다. Spring Java 기반으로 혼자 스스로 만든 사이트 이고, 아버지 회사소개 사이트 이기 때문에 더욱더 신경 써서 만들었습니다. 한달 정도의 기간을 갖고 기획부터 시작해서 개발, 테스트까지 혼자 하였기 때문에 미흡한 점도 있었지만 추후에 시간을 내서 프로젝트의 완성도를 높이고 싶습니다."
+			},{
+			 path: "media/",
+			 url : "personal.mp4", 
+			 title: "Personal",
+			 name: "Media",
+			 img: "personal/PersonalMedia.png",
+			 type : false, 
+			 contents: ""
+			}
+		];
+		
+		$scope.btnEvnet = function(index){
+			$scope.projectUrl = "";
+			$scope.projectFlag = false;
+			
+			for(var i = 0; i < $scope.btnList.length; i++){
+				$scope.btnList[i].active = false;
+			}
+			$scope.btnList[index].active = true;
+			$scope.grid.isotope({ filter: $scope.btnList[index].filter });
+		}
+		
+		setTimeout(function(){
+			$scope.grid = $('#portfolioGroup').isotope();
+		}, 200);
+	});
+	gApp.directive('resize', function ($window) {
+	    return function (scope, element) {
+	        var w = angular.element($window);
+	        scope.getWindowDimensions = function () {
+	            return {
+	                'h': w.height(),
+	                'w': w.width()
+	            };
+	        };
+	        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+	            if(newValue.w >= 768){
+					scope.htmlCheck = false;
+					scope.bodyCheck = false;
+					scope.btCheck = false;
+				}
+	        }, true);
+
+	        w.bind('resize', function () {
+	            scope.$apply();
+	        });
+	    }
+	});
